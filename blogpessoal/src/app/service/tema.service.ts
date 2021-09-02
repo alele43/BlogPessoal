2467fee9e1718465af7d5c64a46a3b4517dab4e8
Alessandra <alessandrasousa_43@hotmail.com>
2021-09-02 12:34:48 -0300

    Implementação do Put e Delete do Tema

--- blogpessoal/src/app/service/tema.service.ts
+++ blogpessoal/src/app/service/tema.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { identity, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
 
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://alessandrasousa.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://alessandrasousa.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://alessandrasousa.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://alessandrasousa.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://alessandrasousa.herokuapp.com/tema/${id}`, this.token)
   }
 
 }
